ab3825b32034aaa1b9553a8d9b05a26fe2fec370
Ky13 <sky_io@outlook.com>
2021-03-22 09:55:21 +0800

    add command line parser for socks-bob

--- Socks-Bob/main.cpp
+++ Socks-Bob/main.cpp
@@ -1,13 +1,36 @@
-#include <iostream>
-#include "add.h"
+#include <QCoreApplication>
+#include <QCommandLineParser>
+#include "tcpserver.h"
 
-using namespace std;
+int main(int argc, char *argv[]) {
+  QCoreApplication a(argc, argv);
+  QCommandLineParser parser;
+  parser.addHelpOption();
+  parser.addOptions({
+        {{"i", "ip"},
+            QCoreApplication::translate("main", "ip address of Socks-Bob"),
+            QCoreApplication::translate("main", "ip")},
+        {{"p", "port"},
+            QCoreApplication::translate("main", "port of Socks-Bob"),
+            QCoreApplication::translate("main", "port")},
+        {{"k", "key"},
+            QCoreApplication::translate("main", "shared secret between Alice and Bob"),
+            QCoreApplication::translate("main", "key")},
+     });
+  parser.process(a);
+  QString str_ip = parser.value("ip"), str_port = parser.value("port"), str_key = parser.value("key");
+  if (str_ip.isEmpty()) str_ip = "0.0.0.0";
+  if (str_port.isEmpty()) str_port = "1082";
+  if (str_key.isEmpty()) str_key = "sky-io";
+  QString serverAddr(str_ip);
+  quint16 serverPort  = str_port.toUShort();
+  QString method = "chacha20-ietf-poly1305";
+  method = "aes-128-gcm";
+  method = "aes-128-ctr";
+  // std::string password = str_key.toStdString();
+  TcpServer tcpserver(30, false, serverAddr, serverPort, method.toStdString(), str_key.toStdString());
+  qDebug() << "hello from Bob (Server Side)" << ", listening at" << (str_ip + ":" + str_port);
+  tcpserver.listen(serverAddr, serverPort);
 
-int main(int argc, char const *argv[])
-{
-    int a, b;
-    cin >> a >> b;
-    cout << add(a, b) << endl;
-    return 0;
+  return a.exec();
 }
-
